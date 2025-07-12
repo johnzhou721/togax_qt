55d20120214cd56711bb6757ab07dd5a0b48ff5d
John Zhou <john-xyz@outlook.com>
2025-07-12 14:55:52 -0500

    [vcs] ignore generated files

--- build/lib/togax_qt/icons.py
+++
@@ -1,9 +0,0 @@
-# Stub impl
-
-class Icon:
-    EXTENSIONS = [".png", ".bmp", ".ico"]
-    SIZES = None
-
-    def __init__(self, interface, path):
-        self.interface = interface
-        self.path = path
