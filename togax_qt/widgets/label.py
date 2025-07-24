20323365c15910de4dd1f784e160ef08a8abfc06
John Zhou <john-xyz@outlook.com>
2025-07-23 21:53:06 -0500

    no line wrapping, correct height, freedom units working!

--- togax_qt/widgets/label.py
+++ togax_qt/widgets/label.py
@@ -8,7 +8,6 @@ from .base import Widget
 class Label(Widget):
     def create(self):
         self.native = QLabel()
-        self.native.setWordWrap(True)
 
     def set_text_align(self, value):
         alignment = {
@@ -23,21 +22,15 @@ class Label(Widget):
 
     def set_font(self, font):
         pass
-    
-    def update_height(self):
-        self.interface.intrinsic.height = self.native.heightForWidth(
-            self.native.width()
-        )
 
     def get_text(self):
         return self.native.text()
 
     def set_text(self, value):
         self.native.setText(value)
-        self.update_height()
         self.refresh()
 
     def rehint(self):
         content_size = self.native.sizeHint()
         self.interface.intrinsic.width = at_least(content_size.width())
-        self.update_height()
+        self.interface.intrinsic.height = content_size.height()
